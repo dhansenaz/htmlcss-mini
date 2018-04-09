ce3dcbe4346ef8da51295089432b682c53232ed9
dhansenaz <dhansen@allthingsdot.com>
2018-04-09 11:15:07 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="main-nav">
+        <div className="logo">Start Bootstrap</div>
+        <div className="links">Menu</div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
